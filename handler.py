45775a4c21a1bf4a99302aa4ace3c9f088546a05
Elvin Valerian <elvinvalerian@MacBook-Air.local>
2026-06-30 10:33:25 +0700

    feat: switch to v22 logic

--- handler.py
+++ handler.py
@@ -4,8 +4,7 @@ import sys
 import boto3
 from faster_whisper import WhisperModel
 
-# Load model di luar handler agar permanen di GPU
-print("LOG: Loading Model...")
+print("LOG: Init Model...")
 model = WhisperModel("small", device="cuda", compute_type="float16")
 
 s3 = boto3.client(
@@ -17,25 +16,21 @@ s3 = boto3.client(
 )
 
 def handler(event):
-    print("--- LOG: HANDLER V21 PROSES AI ---")
+    print("--- LOG: HANDLER V22 START ---")
     job_input = event.get("input", {})
     s3_key = job_input.get("s3_key")
     bucket_name = os.environ.get("R2_BUCKET_NAME")
     
     local_path = f"/tmp/{os.path.basename(s3_key)}"
     
-    # 1. Download
     print(f"LOG: Downloading {s3_key}...")
     s3.download_file(bucket_name, s3_key, local_path)
     
-    # 2. Transkripsi
     print(f"LOG: Transcribing...")
     segments, _ = model.transcribe(local_path, beam_size=5)
     transcription = [{"start": s.start, "end": s.end, "text": s.text} for s in segments]
     
-    # 3. Cleanup
     if os.path.exists(local_path): os.remove(local_path)
-        
     return {"status": "success", "transcription": transcription}
 
 if __name__ == "__main__":
